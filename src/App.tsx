43734000a044efdca80dffd15e4a777d2ac80488
Laurent Vuillaume <figura.graphik@gmail.com>
2023-09-22 00:37:16 +0200

    ADD pokemonList in App component And add props pokemon to PokemonCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css'
 import React from 'react';
 import { PokemonCard } from './components/PokemonCard';
+import { Pokemon } from './types/index';
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const App:React.FC = () => {
   return <>
@@ -8,7 +19,9 @@ const App:React.FC = () => {
       My futur Pokedex app !
     </h1>
     <div>
-      <PokemonCard />
+      { pokemonList.map(
+        (p: Pokemon, i: number) => <PokemonCard key={`pokemon_${i}`} pokemon={p} />
+      )}
     </div>
   </>
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,27 +1,13 @@
 import React from "react"
 import styles from './PokemonCard.module.css';
-type Pokemon = {
-    name?: string,
-    imgSrc?: string;
-}
+import { Pokemon } from "../types";
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-];
-
-export const PokemonCard: React.FC = () => {
-    const pokemon:Pokemon = pokemonList[1]
+export const PokemonCard: React.FC<{pokemon: Pokemon}> = ({pokemon}) => {
+    
     // const pokemon2 = pokemonList[1]
     return <figure className={styles["pokemon-card"]}>
-        {pokemon?.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon?.name ? pokemon.name : "???"} /> : "???"}
-        {pokemon?.name ? <figcaption>{pokemon.name}</figcaption> : <p>???</p>}
+        {pokemon?.imgSrc ? <img src={pokemon?.imgSrc} alt={pokemon?.name ? pokemon?.name : "???"} /> : "???"}
+        {pokemon?.name ? <figcaption>{pokemon?.name}</figcaption> : <p>???</p>}
     </figure>
 }
        
