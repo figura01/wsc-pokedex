e7b6ebb45bac07a046b76c096647160f415039f7
Laurent Vuillaume <figura.graphik@gmail.com>
2023-09-21 21:44:41 +0200

    UPDATE PokemonCard component and ts-node package

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,27 @@
 import React from "react"
 import styles from './PokemonCard.module.css';
+type Pokemon = {
+    name?: string,
+    imgSrc?: string;
+}
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+];
+
 export const PokemonCard: React.FC = () => {
+    const pokemon:Pokemon = pokemonList[1]
+    // const pokemon2 = pokemonList[1]
     return <figure className={styles["pokemon-card"]}>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-        <figcaption>bulbasaur</figcaption>
+        {pokemon?.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon?.name ? pokemon.name : "???"} /> : "???"}
+        {pokemon?.name ? <figcaption>{pokemon.name}</figcaption> : <p>???</p>}
     </figure>
 }
+       
