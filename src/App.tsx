53ac89f12aa4511b4ad551a7fd6fa48888e8eb4b
Laurent Vuillaume <figura.graphik@gmail.com>
2023-09-23 17:57:52 +0200

    Add state and button on app, implement logic on button and mange pokemonIndex, and some style for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
-import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import { PokemonCard } from './components/PokemonCard';
-import { Pokemon } from './types/index';
+import styles from './App.module.css';
+
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 
 const App:React.FC = () => {
-  return <>
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  return <div className={styles.app}>
     <h1>
       My futur Pokedex app !
     </h1>
-    <div>
-      { pokemonList.map(
-        (p: Pokemon, i: number) => <PokemonCard key={`pokemon_${i}`} pokemon={p} />
-      )}
+    <div className={styles.container}>
+      <button disabled={pokemonIndex < 1} onClick={() => setPokemonIndex(pokemonIndex-1)}>Précédent</button>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <button disabled={pokemonIndex >= pokemonList.length-1}onClick={() => setPokemonIndex(pokemonIndex+1)}>Suivant</button>
     </div>
-  </>
+  </div>
 }
 
 export default App
